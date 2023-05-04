4d7844d8cb1298368d5a6d1ddccb35415aff044b
jorgegutierrez <jegutierrezc@gmail.com>
2023-05-05 00:06:34 +0200

    UPDATE: Added new println

--- src/main/java/org/uax/devops/App.java
+++ src/main/java/org/uax/devops/App.java
@@ -6,8 +6,8 @@ package org.uax.devops;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println( "DevOps rocks!" );
     }
 }
